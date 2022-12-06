4538540f452f397bf23806e50795501a7a8acd1f
Lou Lebohec <llebohec@macllebohec.lan>
2022-12-05 22:39:20 -0700

    completed Dec5

--- Dec5/loading_crane/src/main.rs
+++ Dec5/loading_crane/src/main.rs
@@ -23,7 +23,7 @@ fn main() {
     }
     //parsing to just the data needed
     let mut parsed_lines:Vec< Vec<char> > = Vec::new();
-    for line in crates{
+    for line in &crates{
        let attempt= line.chars();
        let mut parsed_line: Vec<char> = Vec::new();
        for (i,item) in attempt.enumerate(){
@@ -39,33 +39,70 @@ fn main() {
 
     for i in 1..9{
         for (j,item) in parsed_lines[parsed_lines.len()-i].iter().enumerate(){
+            if !item.is_whitespace(){
+             stacks[j].push(*item);
+            }
             
-            stacks[j].push(*item);
+            
         }
     }
 
-    for stack in stacks{
-        for item in stack{
-            print!("{}",item)
-        }
-        println!();
-    }
+    //Clone stacks for part two
+    let mut stacks_p2 = stacks.clone();
 
-    //parsing instructions
+    //executing instructions
 
-    let mut parsed_instructions:Vec<Vec<i32>> = Vec::new();
+    
 
     for line in instructions{
-        let mut instruction:Vec<i32> = Vec::new();
+        let mut instruction:Vec<usize> = Vec::new();
         for (j,word) in line.split(" ").enumerate(){
             if j%2==1 {
                 instruction.push(word.parse().unwrap());
             }
         }
-        parsed_instructions.push(instruction);
+        
+        move_crate(&mut stacks,instruction[2],instruction[1],instruction[0], true);
+        move_crate(&mut stacks_p2,instruction[2],instruction[1],instruction[0], false);
+    }
+    println!();
+    for stack in &stacks{
+        for item in stack{
+            print!("{}",item);
+        }
+        println!();
+    }
+    println!();
+    for stack in &stacks_p2{
+        for item in stack{
+            print!("{}",item);
+        }
+        println!();
     }
 }
 
-fn move_crate<T>(vector:Vec<Vec<T>>,origin:i32,target:i32,iterations:i32){
+fn move_crate(vector:&mut Vec<Vec<char>>,target:usize,origin:usize,iterations:usize,part_1:bool){
+    let mut part_2 = String::from("");
+    for _i in 0..iterations{
+        
+        if !vector[origin-1].is_empty(){
+            let crane = vector[origin-1].pop().unwrap();
+            if part_1{vector[target-1].push(crane);}
+            else {part_2.push(crane)}
+        }
+    }
 
+    if !part_1{
+        let moved = part_2.chars();
+        let mut moved_vec:Vec<char> = Vec::new();
+        for cha in moved{
+            moved_vec.push(cha);
+        }
+        moved_vec.reverse();
+
+        for item in moved_vec{
+            vector[target-1].push(item);
+        }
+    }
+    
 }
