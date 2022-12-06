8becdc1f183d7f49e600d0158da4f4dc4670803a
Lou Lebohec <llebohec@macllebohec.lan>
2022-12-05 20:07:49 -0700

    all data parsed appropriately. Moving on to move_crate helper method

--- Dec5/loading_crane/src/main.rs
+++ Dec5/loading_crane/src/main.rs
@@ -21,7 +21,7 @@ fn main() {
             instructions.push(line);
         }
     }
-
+    //parsing to just the data needed
     let mut parsed_lines:Vec< Vec<char> > = Vec::new();
     for line in crates{
        let attempt= line.chars();
@@ -29,13 +29,43 @@ fn main() {
        for (i,item) in attempt.enumerate(){
             if i%4==1{
                 parsed_line.push(item);
-                print!("{}", item);
             }
-       }
-       
-       println!("{}",parsed_line.len());
-
+        }
        parsed_lines.push(parsed_line);
-       
     }
+
+    //Rotation oclock
+    let mut stacks:Vec<Vec<char>>=vec![Vec::new();9];
+
+    for i in 1..9{
+        for (j,item) in parsed_lines[parsed_lines.len()-i].iter().enumerate(){
+            
+            stacks[j].push(*item);
+        }
+    }
+
+    for stack in stacks{
+        for item in stack{
+            print!("{}",item)
+        }
+        println!();
+    }
+
+    //parsing instructions
+
+    let mut parsed_instructions:Vec<Vec<i32>> = Vec::new();
+
+    for line in instructions{
+        let mut instruction:Vec<i32> = Vec::new();
+        for (j,word) in line.split(" ").enumerate(){
+            if j%2==1 {
+                instruction.push(word.parse().unwrap());
+            }
+        }
+        parsed_instructions.push(instruction);
+    }
+}
+
+fn move_crate<T>(vector:Vec<Vec<T>>,origin:i32,target:i32,iterations:i32){
+
 }
