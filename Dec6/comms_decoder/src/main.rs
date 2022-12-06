5e9800669eed57190f0969b4dd0caa5c87e0d031
Lou Lebohec <llebohec@macllebohec.lan>
2022-12-06 15:17:06 -0700

    Completed Dec6

--- Dec6/comms_decoder/src/main.rs
+++ Dec6/comms_decoder/src/main.rs
@@ -7,46 +7,45 @@ fn main() {
     let contents: &str = &fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    let mut last_three:Vec<char> = Vec::new();
+    let mut last_four:Vec<char> = Vec::new();
+    let mut last_fourteen:Vec<char> = Vec::new();
     let mut part_1_result = 0;
-    
+    let mut part_2_result = 0;
+    let mut done = false;
+    let mut done_2 = false;
 
     for cha in contents.chars(){
-        let mut done = true;
-        part_1_result += 1;
+        last_four.push(cha);
+        last_fourteen.push(cha);
+        if !done{part_1_result += 1;}
+        if !done_2{part_2_result += 1;}
 
-        if last_three.len()>0{
-            let mut i = 0;
-            for item in &last_three{
-                i+=1;
-                if item == &cha {
-                    
-                    for _j in 0..i{
-                        if last_three.len()>0{last_three.remove(0);}
-                        for item in &last_three{print!("{}",item);}
-                println!("{}", cha);
-                    }
-
-                    done=false;
-                    break;
-                }
-
-                
-            }
-            
-            
-            if done && last_three.len()>=3{
-                println!("{}", part_1_result);
-                for item in &last_three{print!("{}",item);}
-                println!("{}", cha);
-                break;
-            }
-
-            if last_three.len()>0{last_three.remove(0);}
-            
+        if !done {
+            done = check_packet(&last_four,4);
         }
+        done_2 = check_packet(&last_fourteen,14);
 
-        last_three.push(cha);
-        
+        if done && done_2 {break;}
+        if last_four.len()==4{last_four.remove(0);}
+        if last_fourteen.len()==14{last_fourteen.remove(0);}
     }
+
+    println!("Part 1: {} -> Part 2: {}",part_1_result,part_2_result);
 }
+
+fn check_packet(packet:&Vec<char>,length: usize)->bool{
+    
+    if packet.len()==length{
+        for i in 0..length{
+            for j in 0..length{
+                if i != j && packet[i] == packet[j]{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    else{
+        return false;}
+}
+
